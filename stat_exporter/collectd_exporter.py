33599bb84e80e969a317f0605db320fccc2907d1
deepamartin <deepa.martin@maplelabs.com>
2018-12-05 16:52:44 +0530

    Update collectd_exporter.py
    added ozzie, yarn,namenode in list which send list of documents

--- stat_exporter/collectd_exporter.py
+++ stat_exporter/collectd_exporter.py
@@ -132,7 +132,7 @@ def get_stats_dir():
 #     return data
 
 def get_data(num_samples=1, plugins=None, plugin_instances=None):
-    multi_plugins = ["linux", "mysql", "jvm", "tpcc", "postgres", "kafkatopic", "kafkajmx", "zookeeperjmx", "apache", "topstats", "nginx", "cassandra", "hxconnect", "esalogstore"]
+    multi_plugins = ["linux", "mysql", "jvm", "tpcc", "postgres", "oozie","yarn","namenode","kafkatopic", "kafkajmx", "zookeeperjmx", "apache", "topstats", "nginx", "cassandra", "hxconnect", "esalogstore"]
     if plugin_instances is None:
         plugin_instances = [ALL]
     if plugins is None:
