14b39fbfe73f88b34735696739dbcb3226b87757
deepamartin <deepa.martin@maplelabs.com>
2019-01-02 13:18:56 +0530

    Update collectd_exporter.py
    Added tomcat to the list of plugins thats  creates sub folders under plugin

--- stat_exporter/collectd_exporter.py
+++ stat_exporter/collectd_exporter.py
@@ -132,7 +132,7 @@ def get_stats_dir():
 #     return data
 
 def get_data(num_samples=1, plugins=None, plugin_instances=None):
-    multi_plugins = ["linux", "redisdb", "mysql", "jvm", "tpcc", "postgres", "oozie","yarn","namenode","kafkatopic", "kafkajmx", "zookeeperjmx", "apache", "topstats", "nginx", "nginxplus", "cassandra", "hxconnect", "esalogstore"]
+    multi_plugins = ["linux", "redisdb", "mysql", "jvm", "tpcc", "tomcat","postgres", "oozie","yarn","namenode","kafkatopic", "kafkajmx", "zookeeperjmx", "apache", "topstats", "nginx", "nginxplus", "cassandra", "hxconnect", "esalogstore"]
     if plugin_instances is None:
         plugin_instances = [ALL]
     if plugins is None:
